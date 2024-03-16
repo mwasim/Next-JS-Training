a73d5adf51ad1ae9cb010b494d4fac7c53f6df6b
Wasim <mwasimse@gmail.com>
2024-03-16 17:11:02 +0500

    NextJS-Training: 1). Hero component created as reusable cover image for different pages. 2). styling of Hero component using Tailwind 3). Used Hero component on the Home page.

--- corp-demo/src/app/page.tsx
+++ corp-demo/src/app/page.tsx
@@ -1,19 +1,13 @@
-import Image from "next/image"; //built-in Image library does the image resizing/compression at run time for different resolutions.
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <>
-      <div>
-        Home Page!
-        <div className="absolute -z-10 inset-0">
-          <Image
-            src="/images/home.jpg"
-            alt="Car factory"
-            fill
-            style={{ objectFit: `cover` }}
-          />
-        </div>
-      </div>
+      <Hero
+        imgPath="/images/home.jpg"
+        imgAlt="Car Factory"
+        title="Professional Cloud Hosting"
+      />
     </>
   );
 }
---
+++ corp-demo/src/components/hero.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image"; //built-in Image library does the image resizing/compression at run time for different resolutions.
+import type { StaticImageData } from "next/image";
+
+interface HeroProps {
+  imgPath: string;
+  imgAlt: string;
+  title: string;
+}
+
+const Hero = (props: HeroProps) => {
+  return (
+    <div className=" relative h-screen">
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={props.imgPath}
+          alt={props.imgAlt}
+          title={props.title}
+          fill
+          style={{ objectFit: `cover` }}
+        />
+        <div className="absolute inset-0 bg-gradient-to-r from-slate-900" />
+      </div>
+      <div className="pt-48 flex justify-center items-center">
+        <h1 className="text-white text-6xl">{props.title}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
