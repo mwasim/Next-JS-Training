334be49db21a0ee1ff7f8cdc1cc1941cc641ee31
Wasim <mwasimse@gmail.com>
2024-04-18 18:52:47 +0500

    SnippetsManager - Implemented custom 404 page when snippet is not found against a particular id.

---
+++ snippets-manager-app/src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+const SnippetNotFound = () => {
+  return (
+    <h1 className="text-xl bold">
+      Sorry, but we could not find that particular snippet!
+    </h1>
+  );
+};
+
+export default SnippetNotFound;
