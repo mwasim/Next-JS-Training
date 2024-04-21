d2934dd8d52192e50bbb7aa3223d20b13391b037
Wasim <mwasimse@gmail.com>
2024-04-21 16:58:31 +0500

    SnippetManager - implemented automatic loading spinner and tested by adding artificial delay

--- snippets-manager-app/src/app/snippets/[id]/page.tsx
+++ snippets-manager-app/src/app/snippets/[id]/page.tsx
@@ -9,6 +9,10 @@ interface SnippetShowPageProps {
 
 const SnippetShowPage = async (props: SnippetShowPageProps) => {
   //console.log(props);
+
+  //Add artificial delay to test the loading.tsx page
+  await new Promise((r) => setTimeout(r, 2000));
+
   const snippet = await db.snippet.findFirst({
     where: {
       id: parseInt(props.params.id),
