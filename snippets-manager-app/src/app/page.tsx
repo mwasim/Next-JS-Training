f07bffb48f9332f1b5f223bcaa21872ce1e0dba4
Wasim <mwasimse@gmail.com>
2024-04-21 17:09:42 +0500

    SnippetsManager - Home page design improved and added links and buttons to create a new snippet and view existing snippets.

--- snippets-manager-app/src/app/page.tsx
+++ snippets-manager-app/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { db } from "@/db";
 /*
   By default all components in NEXT.js are SERVER COMPONENTS unless marked as `use client`
@@ -7,8 +8,27 @@ export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <Link
+        key={snippet.id}
+        href={`/snippets/${snippet.id}`}
+        className="flex justify-between items-center p-2 border rounded"
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href={`/snippets/new`} className="borer p-2 border rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
