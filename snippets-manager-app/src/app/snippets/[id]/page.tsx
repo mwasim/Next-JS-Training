30b8d1e13bce3e8af722d970b108927e3eb2abe8
Wasim <mwasimse@gmail.com>
2024-04-24 18:42:36 +0500

    SnippetsManager - Edit Snippet Page created with initial version. 2). Clicking the Edit button on Show Snippet page opens the Edit Snippet page.

--- snippets-manager-app/src/app/snippets/[id]/page.tsx
+++ snippets-manager-app/src/app/snippets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import { db } from "@/db";
+import Link from "next/link";
 
 interface SnippetShowPageProps {
   params: {
@@ -28,7 +29,12 @@ const SnippetShowPage = async (props: SnippetShowPageProps) => {
       <div className="flex m-4 justify-between items-center">
         <h1 className="text-xl font-bold"> {snippet.title}</h1>
         <div className="flex gap-4">
-          <button className="p-2 border rounded">Edit</button>
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className="p-2 border rounded"
+          >
+            Edit
+          </Link>
           <button className="p-2 border rounded">Delete</button>
         </div>
       </div>
