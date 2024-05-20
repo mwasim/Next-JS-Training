7696c4640020058d0d572b7ad4fa89d3fb32b6b3
Wasim <mwasimse@gmail.com>
2024-05-20 15:54:54 +0500

    SnippetsManager - implemented handleEditorChange event handler to update the code snippet on making changes to the code editor.

--- snippets-manager-app/src/components/snippet-edit-form.tsx
+++ snippets-manager-app/src/components/snippet-edit-form.tsx
@@ -2,11 +2,17 @@
 
 import type { Snippet } from "@prisma/client";
 import { Editor } from "@monaco-editor/react";
+import { useState } from "react";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 const SnippetEditForm = ({ snippet }: SnippetEditFormProps) => {
+  const [code, setCode] = useState(snippet.code);
+  function handleEditorChange(value: string = ""): void {
+    setCode(value);
+  }
+
   return (
     <div>
       <Editor
@@ -15,6 +21,7 @@ const SnippetEditForm = ({ snippet }: SnippetEditFormProps) => {
         language="javascript"
         defaultValue={snippet.code}
         options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
       />
     </div>
   );
