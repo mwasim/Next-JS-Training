915a9cea3047e0128b33d2c4f3b52248cf164d2c
Wasim <mwasimse@gmail.com>
2024-05-21 15:27:58 +0500

    SnippetsManager - server actions file created under actions directory. 2). Server action is used in the client component snippet-edit-form to update the code snippet.

---
+++ snippets-manager-app/src/actions/index.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
+export async function editSnippet(id: number, code: string) {
+  console.log(id, code);
+  await db.snippet.update({
+    where: { id },
+    data: { code },
+  });
+
+  redirect(`/snippets/${id}`);
+}
--- snippets-manager-app/src/components/snippet-edit-form.tsx
+++ snippets-manager-app/src/components/snippet-edit-form.tsx
@@ -3,6 +3,7 @@
 import type { Snippet } from "@prisma/client";
 import { Editor } from "@monaco-editor/react";
 import { useState } from "react";
+import * as actions from "@/actions";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
@@ -12,6 +13,8 @@ const SnippetEditForm = ({ snippet }: SnippetEditFormProps) => {
   function handleEditorChange(value: string = ""): void {
     setCode(value);
   }
+
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
 
   return (
     <div>
@@ -23,6 +26,12 @@ const SnippetEditForm = ({ snippet }: SnippetEditFormProps) => {
         options={{ minimap: { enabled: false } }}
         onChange={handleEditorChange}
       />
+
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded">
+          Save
+        </button>
+      </form>
     </div>
   );
 };
